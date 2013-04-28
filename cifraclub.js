dbf114d3de5ed4883a4201c37dace001d9d2de5e
Rodrigo Souza <rodrigorgs@gmail.com>
2013-04-28 16:54:30 -0300

    Initial version (pre-alpha).

---
+++ cifraclub.js
@@ -0,0 +1,81 @@
+function genSpaces(len, character) {
+	if (!character)
+		character = " "
+	return new Array(len + 1).join(character);
+}
+
+var cifraElem = document.getElementById("ct_cifra");
+
+var head = document.getElementsByTagName("head")[0];
+head.innerHTML = "<title>CifraTV</title>";
+
+var body = document.body;
+body.innerHTML = "";
+
+var cifra = cifraElem.innerHTML;
+
+// remove tablature
+cifra = cifra.replace(/<span class="tablatura">(.|[\r\n])*?<\/span><\/span>/gm, '');
+
+// simplify chord tags
+cifra = cifra.replace(/<b .*?>/g, "<b>");
+
+var lines = cifra.split("\n");
+lines.push("");
+
+var linesToDelete = [];
+
+for (var i = 0; i < lines.length; i++) {
+	var pos = lines[i].lastIndexOf("<b>");
+	while (pos != -1) {
+		console.log(pos);
+		var subline = lines[i].substr(pos);
+		var posEnd = pos + subline.indexOf("</b>") + 3;
+		var chord = lines[i].substr(pos + 3, posEnd - pos - 6);
+		console.log(chord);
+
+		// erase chord from the line
+		lines[i] = lines[i].substr(0, pos) + 
+				genSpaces(chord.length, " ") + 
+				lines[i].substr(posEnd + 1);
+
+		// insert chord into next line (extend the line if it's small)
+
+		// extend line below with empty spaces
+		if (lines[i+1].length < pos)
+			lines[i+1] += genSpaces(pos - lines[i+1].length);
+
+		// compute real pos (excluding tags)
+		var before = lines[i].substr(0, pos);
+		before = before.replace(/<.*?>/g, '');
+		var realPos = before.length;
+		lines[i+1] = lines[i+1].substr(0, realPos) + 
+				"<span class='chord'>" + chord + "</span>" + 
+				lines[i+1].substr(realPos);
+
+		pos = lines[i].lastIndexOf("<b>");
+		if (pos == -1 && lines[i].replace(/\s+/, '').length == 0) {
+			// this is a line that used to have chords, now it's empty
+			linesToDelete.push(i);
+		}
+	}
+}
+
+// remove lines that contained only chords
+for (var i = linesToDelete.length - 1; i >= 0; i--) {
+	var toDelete = linesToDelete[i];
+	lines.splice(toDelete, 1);
+}
+
+// Add paragraph and paragraph style
+for (var i = 0; i < lines.length; i++) {
+	lines[i] = "<p>" + lines[i] + "</p>";
+}
+
+cifra = lines.join("\n");
+console.log(cifra);
+var newCifraElem = document.createElement("div");
+newCifraElem.id = cifraElem.id;
+newCifraElem.innerHTML = cifra;
+body.appendChild(newCifraElem);
+
---
+++ cifratv.js
@@ -0,0 +1,11 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.insertCSS(tab.id, {file: "tv.css"});
+	chrome.tabs.executeScript(null, {file: "cifraclub.js"});
+
+	// TODO: check if it's a cifra page
+    // var regexPage = new RegExp(/https:\/\/www.google.com.mx\//); // We use a regular expresion to check which page was given.
+    // var match = regexPage.exec(tab.url); // We then check if the given page matches our expression.
+    // // If it matches and the status of the tab is complete...
+    // if(match && tab.status === 'complete') {
+
+});
